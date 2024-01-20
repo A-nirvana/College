79d1ee187e1acc924e0a2a81ef4033dfa2e02b3b
hirakRajbonshi <hrkraj0@gmail.com>
2024-01-20 12:20:05 +0530

    Add Files

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { Navbar } from "./components"
+import {Home, Error} from './pages'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/*" element={<Error/>}/>
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import './Navbar.css'
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="logo-container">
+            
+            </div>
+            <div className="hamburger-menu">asdfjkb</div>
+            <div className="nav-options">
+                <div className="nav-option">
+                    <h1>Home</h1>
+                </div>
+                <div className="nav-option">
+                    <h1>About</h1>
+                </div>
+                <div className="nav-option">
+                    <h1>Events</h1>
+                </div>
+                <div className="nav-option">
+                    <h1>Contact</h1>
+                </div>
+            </div>
+        </nav>
+    )
+}
